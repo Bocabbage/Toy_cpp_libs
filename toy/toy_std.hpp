86b33e6ff08121eebb8f1fb715e8a4b9602b6274
Bocabbage <378066505@qq.com>
2019-11-22 20:26:08 +0800

    Update: Add Namespace; Rename.

---
+++ toy/toy_std.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include<iostream>
+#include<memory>
+#include<stdexcept>
+
+namespace toy_std
+{
+    /*  
+        It's the namespace -- toy_std, which is my exercise-codes of C++.
+    */
+}
