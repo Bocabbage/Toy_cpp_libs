7fb994e52bfd1ad2bfd8afd701df2aea3a8fbdae
Bocabbage <378066505@qq.com>
2019-12-03 16:59:53 +0800

    Update: allocator(unfinished).

--- src/toy_memory/toy_stl_alloc.hpp
+++ src/toy_memory/toy_stl_alloc.hpp
@@ -1,7 +1,9 @@
 /*
     Project:        Toy_STL_Construct
-    Update date:    2019/12/02
+    Update date:    2019/12/03
     Author:         Zhuofan Zhang
+
+    Update Log:     2019/12/03 -- Add 'allocate/deallocate/refill' of the sub-allocator
 */
 #pragma once
 #include "../toy_std.hpp"
@@ -29,12 +31,12 @@ namespace toy_std
     };
 
     // Initialize the new_handler with 'nullptr'
-    void (*__malloc_alloc::__malloc_alloc_oom_handler)() = nullptr;
+    void (*__malloc_alloc::__malloc_alloc_oom_handler)() = 0;
 
     void* __malloc_alloc::allocate(size_t n)
     {
         void* result = malloc(n);
-        if (result == nullptr)
+        if (result == 0)
             result = oom_malloc(n);
         return result;
     }
@@ -47,7 +49,7 @@ namespace toy_std
         while (true)
         {
             my_malloc_handler = __malloc_alloc_oom_handler;
-            if (my_malloc_handler == nullptr)
+            if (my_malloc_handler == 0)
                 throw std::bad_alloc();
             result = malloc(n);
             if (result)
@@ -73,11 +75,17 @@ namespace toy_std
             char client_data[1];        // The client sees this.
         };
 
-        // 16 free-lists
+        /* 
+            16 free-lists 
+            manage blocks size of:
+            8,16,24,32,40,48,56,64,72,80,88,96,104,112,120,128
+        */
         static obj* volatile free_list[__NFREELISTS];
 
         static size_t FREELIST_INDEX(size_t bytes)
         {
+            // Decide which free_list to be used according to
+            // the block size.
             return ((bytes + __ALIGN - 1) / __ALIGN - 1);
         }
 
@@ -101,4 +109,77 @@ namespace toy_std
     __default_alloc::obj* volatile __default_alloc::free_list[__NFREELISTS] =
     { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
 
+    char* __default_alloc::chunk_alloc(size_t n, int& nobjs)
+    {
+        // Memory Pool
+    }
+
+    void* __default_alloc::allocate(size_t n)
+    {
+        if (n > __MAX_BYTES)
+            return __malloc_alloc::allocate(n);
+
+        obj* volatile* my_free_list; // pointer of pointer
+        obj* result;
+
+        my_free_list = free_list + FREELIST_INDEX(n);
+        result = *my_free_list;
+        if (result == 0)
+        {
+            // no free-mem could be used; refill the free-list.
+            void* r = refill(ROUND_UP(n));
+            return r;
+        }
+    
+        // Modify the free list
+        *my_free_list = result->free_list_link;
+        return result;
+    }
+
+    void __default_alloc::deallocate(void* p, size_t n)
+    {
+        if (n > __MAX_BYTES)
+        {
+            __malloc_alloc::deallocate(p);
+            return;
+        }
+        
+        obj* q = (obj*) p;
+        obj* volatile* my_free_list = free_list + FREELIST_INDEX(n);
+        
+        q->free_list_link = *my_free_list;
+        *my_free_list = q;
+
+    }
+
+    void* __default_alloc::refill(size_t n)
+    {
+        // default: get 20 new blocks
+        int nobjs = 20;
+        char* chunk = chunk_alloc(n, nobjs);
+        obj* volatile* my_free_list;
+        obj* result;
+        obj* current_obj, * next_obj;
+        int i;
+
+        if (nobjs == 1)return chunk;
+        my_free_list = free_list + FREELIST_INDEX(n);
+        
+        result = (obj*)chunk;
+        *my_free_list = next_obj = (obj*)(chunk + n);
+
+        for (i = 1;; i++)
+        {
+            current_obj = next_obj;
+            next_obj = (obj*)((char*)next_obj + n);
+            if (nobjs - 1 == i)
+            {
+                current_obj->free_list_link = 0;
+                break;
+            }
+            else
+                current_obj->free_list_link = next_obj;
+        }
+        return result;
+    }
 }
