b9494dc412ed67a26c8f1ac57d34cfdf216920ff
Bocabbage <378066505@qq.com>
2020-01-07 23:26:44 +0800

    Update tlist(unfinished).

--- src/containers/toylist.hpp
+++ src/containers/toylist.hpp
@@ -1,12 +1,14 @@
 /*
     Project:        Toy_List
-    Update date:    2019/12/19
+    Update date:    2020/1/7
     Author:         Zhuofan Zhang
 */
 #pragma once
 #include"toy_std.hpp"
 #include"toymemory.hpp"
 #include"toyiterator.hpp"
+
+using std::initializer_list;
 
 namespace toy_std
 {
@@ -64,7 +66,8 @@ namespace toy_std
     };
 
     /* tList */
-    template<typename T,typename Allocator = tallocator<T>>
+    // Noted that the Allocator parameter is not used here.
+    template<typename T, typename Allocator = tallocator<T>>
     class tlist
     {
     public:
@@ -79,7 +82,185 @@ namespace toy_std
         using iterator = __List_Iterator;
         using __tNode_Pointer = __tList_Node<T>*;
 
-    private:
+        /* Constructors */
+        tlist() :__alloc(), __Node(nullptr) {  }
+        tlist(size_type, const value_type&);
+        tlist(const tlist<T, Allocator>&);
+        tlist(tlist<T, Allocator>&&) noexcept;
+        tlist(initializer_list<value_type>);
+        template<typename InputIt>
+        tlist(InputIt first, InputIt last);
+        tlist<T, Allocator>& operator=(const tlist<T, Allocator>&);
+        tlist<T, Allocator>& operator=(tlist<T, Allocator>&&);
+
+        /* Destructor */
+        ~tlist() noexcept;
+
+        /* Iterators */
+
+        /* Modifiers */
+        void swap(tlist<T, Allocator>&);
+        void clear() noexcept;
+        
+
+    protected:
         __tNode_Pointer __Node;
+        tallocator<__tList_Node<T>> __alloc;
     };
+
+    template<typename T, typename Allocator>
+    void
+    tlist<T, Allocator>::swap(tlist<T, Allocator>& t)
+    {
+        std::swap(this->__Node, t.__Node);
+        std::swap(this->__alloc, t.__alloc);
+    }
+
+    /*
+    template<typename T, typename Allocator>
+    void
+    tlist<T, Allocator>::clear() noexcept
+    {
+
+    }
+    */
+
+    template<typename T, typename Allocator>
+    tlist<T, Allocator>::tlist(size_type n, const value_type& value):
+    __alloc(), __Node(nullptr)
+    {
+        __Node = __alloc.allocate(1);
+        __Node->_data = value;
+        auto tmp = __Node;
+        n--;
+        for (size_t i = 0; i < n; i++)
+        {
+            tmp->_next = __alloc.allocate(1);
+            tmp->_next->_prev = tmp;
+            tmp = tmp->_next;
+            tmp->_data = value;
+        }
+
+        __Node->_prev = tmp;
+        tmp->_next = __Node;
+    }
+
+    template<typename T, typename Allocator>
+    tlist<T, Allocator>::tlist(const tlist<T, Allocator>& t):
+    __alloc(), __Node(nullptr)
+    {
+        if (t.__Node == nullptr)
+        {
+            __Node = nullptr;
+            return;
+        }
+
+        __Node = __alloc.allocate(1);
+        __Node->_data = t.__Node->_data;
+        auto tmp = __Node;
+        auto t_tmp = t.__Node;
+        while (t_tmp->_next != t.__Node)
+        {
+            tmp->_next = __alloc.allocate(1);
+            tmp->_next->_prev = tmp;
+            tmp = tmp->_next;
+            t_tmp = t_tmp->_next;
+            tmp->_data = t_tmp->_data;
+        }
+
+        __Node->_prev = tmp;
+        tmp->_next = __Node;
+    }
+
+    template<typename T, typename Allocator>
+    tlist<T, Allocator>::tlist(tlist<T, Allocator>&& rt) noexcept:
+    __alloc(rt.__alloc), __Node(rt.__Node)
+    {
+        rt.__alloc = tallocator<__tList_Node<T>>(); // new empty allocator
+        rt.__Node = nullptr;
+    }
+
+    template<typename T, typename Allocator>
+    tlist<T, Allocator>&
+    tlist<T, Allocator>::operator=(const tlist<T, Allocator>& t)
+    {
+        tlist<T, Allocator> tmp(t);
+        swap(tmp);
+        return *this;
+    }
+
+    template<typename T, typename Allocator>
+    tlist<T, Allocator>&
+    tlist<T, Allocator>::operator=(tlist<T, Allocator>&& rt)
+    {
+        if (this != &rt)
+        {
+            clear();
+            __alloc = rt.__alloc;
+            __Node = rt.__Node;
+
+            rt.__alloc = tallocator<__tList_Node<T>>();
+            rt.__Node = nullptr;
+        }
+
+        return *this;
+    }
+
+    template<typename T, typename Allocator>
+    template<typename InputIt>
+    tlist<T, Allocator>::tlist(InputIt first, InputIt last):
+    __alloc(), __Node(nullptr)
+    {
+        if (first == last)
+        {
+            __Node = nullptr;
+            return;
+        }
+
+        __Node = __alloc.allocate(1);
+        __Node->_data = *first;
+        auto tmp = __Node;
+        first++;
+        while (first != last)
+        {
+            tmp->_next = __alloc.allocate(1);
+            tmp->_next->_prev = tmp;
+            tmp = tmp->_next;
+            tmp->_data = *first;
+            first++;
+        }
+
+        __Node->_prev = tmp;
+        tmp->_next = __Node;
+    }
+
+    template<typename T, typename Allocator>
+    tlist<T, Allocator>::tlist(initializer_list<value_type> ilist):
+    __alloc(), __Node(nullptr)
+    {
+        if (ilist.size() == 0)
+        {
+            __Node = nullptr;
+            return;
+        }
+
+        __Node = __alloc.allocate(1);
+        auto t_tmp = ilist.begin()
+        __Node->_data = *t_tmp;
+        auto tmp = __Node;
+        t_tmp++;
+        while (t_tmp != ilist.end())
+        {
+            tmp->_next = __alloc.allocate(1);
+            tmp->_next->_prev = tmp;
+            tmp = tmp->_next;
+            tmp->_data = *t_tmp;
+            t_tmp++;
+        }
+
+        __Node->_prev = tmp;
+        tmp->_next = __Node;
+    }
+
 }
+
--- src/toymemory.hpp
+++ src/toymemory.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#include "toy_memory/toy_stl_construct.hpp"
-#include "toy_memory/toy_stl_alloc.hpp"
-#include "toy_memory/toy_stl_uninitialized.hpp"
-#include "toy_memory/toyallocator.hpp"
+#include "toy_stl_construct.hpp"
+#include "toy_stl_alloc.hpp"
+#include "toy_stl_uninitialized.hpp"
+#include "toyallocator.hpp"
 
